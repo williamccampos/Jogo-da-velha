34294e471f06d8535b86eee4ffd72ce8e490935d
William Campos <66341532+williamccampos@users.noreply.github.com>
2021-09-19 14:59:40 -0300

    Commt push

---
+++ src/jogoDaVelha/Computador.java
@@ -0,0 +1,19 @@
+package jogoDaVelha;
+
+public class Computador extends Jogador{
+    
+    public Computador(int jogador){
+        super(jogador);
+        System.out.println("Jogador 'Computador' criado!");
+    }
+    
+    @Override
+    public void jogar(Tabuleiro tabuleiro){
+        
+    }
+    
+    @Override
+    public void Tentativa(Tabuleiro tabuleiro){
+        
+    }
+}
---
+++ src/jogoDaVelha/JogoDaVelha.java
@@ -0,0 +1,17 @@
+package jogoDaVelha;
+
+/*
+ * @William Campos
+ * Jogo da Velha em Desenvolvimento
+ */
+
+public class JogoDaVelha {
+
+    public static void main(String[] args) {
+        Jogo jogo = new Jogo();
+        
+        
+    }
+    
+    
+}
